01a63934361e9b5cae061f92ba38ba15f1a26a03
1911915 <1911915@01-18105.pusd.dom>
2026-02-27 11:58:12 -0800

    Added queue and stack methods

--- Q3 Project 1 Period 2/src/Stack.java
+++ Q3 Project 1 Period 2/src/Stack.java
@@ -1,23 +1,25 @@
 import java.util.ArrayList;
 
-public class Queue {
-	
-	private ArrayList<Integer> queueList = new ArrayList<Integer>();
+public class Stack {
+
+	ArrayList<Integer> queueList = new ArrayList<Integer>();
 	private static int index = 0;
 	
-	
-	public static void enqueue(ArrayList<Integer> queue, int o){
+	public static void push(ArrayList<Integer> queue, int o){
 		queue.add(o);
 		index++;
 	}
 	
-	public static void dequeue(ArrayList<Integer> queue){
-		queue.remove(0);
-		index--;
+	public int pop(ArrayList<Integer> queue){
+		return queue.remove(index);
 	}
 	
 	public int peek(){
 		return queueList.get(index);
+	}
+	
+	public static void search(){
+		
 	}
 	
 	public boolean isEmpty(ArrayList<Integer> queue) {
@@ -30,5 +32,5 @@ public class Queue {
 	public int size(ArrayList<Integer> queue){
 		return queue.size();
 	}
-
+	
 }
